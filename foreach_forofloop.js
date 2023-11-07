89ea663c83cc39f7d9e732c369489155f77d7d7b
Chetan-adroit <chetan.karande@theadroit.in>
2023-11-07 15:05:42 +0530

    first-commit

--- foreach_forofloop.js
+++ foreach_forofloop.js
@@ -2,21 +2,28 @@
 const head_clicks = document.querySelectorAll('.accordian-box h3');
 const para_contains = document.querySelectorAll('.contain-para');
 
-// Loop through each h3 element and attach click event listeners
+// Loop through each h3 element and attach mouseover event listeners
 head_clicks.forEach(function(h3Element, index) {
-    h3Element.addEventListener("click", function() {    
-        // Toggle the 'custom_icon' class on the clicked h3 element
-        h3Element.classList.toggle('custom_icon');
+    h3Element.addEventListener("mouseover", function() {
+        // Add the 'custom_icon' class when hovering
+        h3Element.classList.add('custom_icon');
+    });
 
+    h3Element.addEventListener("mouseout", function() {
+        // Remove the 'custom_icon' class when mouseover ends
+        h3Element.classList.remove('custom_icon');
+    });
+
+    h3Element.addEventListener("click", function() {
         // Toggle the 'custom-para' class on the corresponding .contain-para element
         para_contains[index].classList.toggle('custom-para');
 
         // Close other open paragraphs
-        para_contains.forEach(function(para, paraIndex) {
-            if (paraIndex !== index) {
-                para.classList.remove('custom-para');
-            }
-        });
+        // para_contains.forEach(function(para, paraIndex) {
+        //     if (paraIndex !== index) {
+        //         para.classList.remove('custom-para');
+        //     }
+        // });
 
         // Reset styling on other headers
         head_clicks.forEach(function(otherH3, otherIndex) {
@@ -26,4 +33,3 @@ head_clicks.forEach(function(h3Element, index) {
         });
     });
 });
-
